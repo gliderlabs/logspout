7fa24dc71bdb175f0716ada5b9ac5259c0777349
Giorgos Logiotatidis <seadog@sealabs.net>
2015-09-02 14:48:04 +0300

    Add TLS transport. Fixes #116.

--- modules.go
+++ modules.go
@@ -7,4 +7,5 @@ import (
 	_ "github.com/gliderlabs/logspout/routesapi"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/gliderlabs/logspout/transports/tls"
 )
