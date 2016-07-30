f93d1d3df4789c76f695947f4e297724c56b890a
Matt Aitchison <Matt@lanciv.com>
2016-07-30 16:43:12 -0500

    added FakeRoundTripper and container rename test

--- router/pump_test.go
+++ router/pump_test.go
@@ -1,7 +1,11 @@
 package router
 
 import (
+	"io/ioutil"
+	"net"
+	"net/http"
 	"os"
+	"strings"
 	"testing"
 
 	docker "github.com/fsouza/go-dockerclient"
@@ -34,6 +38,38 @@ func TestLogsPumpName(t *testing.T) {
 	if name := p.Name(); name != "pump" {
 		t.Error("name should be 'pump' got:", name)
 	}
+}
+
+func TestContainerRename(t *testing.T) {
+	jsonContainers := `{
+             "Id": "8dfafdbc3a40",
+			 "Name":"bar",
+             "Image": "base:latest",
+             "Command": "echo 1",
+             "Ports":[{"PrivatePort": 2222, "PublicPort": 3333, "Type": "tcp"}],
+             "Status": "Exit 0"
+     }`
+
+	client := newTestClient(&FakeRoundTripper{message: jsonContainers, status: http.StatusOK})
+	p := &LogsPump{
+		client: &client,
+		pumps:  make(map[string]*containerPump),
+		routes: make(map[chan *update]struct{}),
+	}
+	container := &docker.Container{
+		ID:   "8dfafdbc3a40",
+		Name: "foo",
+	}
+	p.pumps["8dfafdbc3a40"] = newContainerPump(container, os.Stdout, os.Stderr)
+	if name := p.pumps["8dfafdbc3a40"].container.Name; name != "foo" {
+		t.Errorf("containerPump should have name: 'foo' got name: '%s'", name)
+	}
+
+	p.rename(&docker.APIEvents{ID: "8dfafdbc3a40"})
+	if name := p.pumps["8dfafdbc3a40"].container.Name; name != "bar" {
+		t.Errorf("containerPump should have name: 'bar' got name: %s", name)
+	}
+
 }
 
 func TestNewContainerPump(t *testing.T) {
@@ -81,4 +117,37 @@ func TestPumpSendTimeout(t *testing.T) {
 		t.Error("expected logstream to be removed after timeout")
 	}
 
+}
+
+type FakeRoundTripper struct {
+	message  string
+	status   int
+	header   map[string]string
+	requests []*http.Request
+}
+
+func (rt *FakeRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	body := strings.NewReader(rt.message)
+	rt.requests = append(rt.requests, r)
+	res := &http.Response{
+		StatusCode: rt.status,
+		Body:       ioutil.NopCloser(body),
+		Header:     make(http.Header),
+	}
+	for k, v := range rt.header {
+		res.Header.Set(k, v)
+	}
+	return res, nil
+}
+func (rt *FakeRoundTripper) Reset() {
+	rt.requests = nil
+}
+
+func newTestClient(rt *FakeRoundTripper) docker.Client {
+	endpoint := "http://localhost:4243"
+	client, _ := docker.NewClient(endpoint)
+	client.HTTPClient = &http.Client{Transport: rt}
+	client.Dialer = &net.Dialer{}
+	client.SkipServerVersionCheck = true
+	return *client
 }
