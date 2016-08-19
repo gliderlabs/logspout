2b205a59d2bd36589dc3fa1a52d2a9f80059b81f
Jonathan VUILLEMIN <ekkinox@gmail.com>
2016-08-19 17:47:07 +0200

    FIX : add build-base package install to fix missing gcc

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
-apk add --update go git mercurial
+apk add --update go git mercurial build-base
 mkdir -p /go/src/github.com/gliderlabs
 cp -r /src /go/src/github.com/gliderlabs/logspout
 cd /go/src/github.com/gliderlabs/logspout
 export GOPATH=/go
 go get
 go build -ldflags "-X main.Version $1" -o /bin/logspout
-apk del go git mercurial
+apk del go git mercurial build-base
 rm -rf /go
 rm -rf /var/cache/apk/*
 
