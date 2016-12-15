fd8fc0e305685cb9fc9ced6e0005b9f251fcf2c3
David Hallum <david@kenzielane.com>
2016-12-15 11:47:00 -0500

    Updating Dockerfile base image to gliderlabs/alpine:3.4 from 3.3 (#242)
    This was done to upgrade the go version and resolve a go build import
    error for go-check

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM gliderlabs/alpine:3.3
+FROM gliderlabs/alpine:3.4
 ENTRYPOINT ["/bin/logspout"]
 VOLUME /mnt/routes
 EXPOSE 80
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cp -r /src /go/src/github.com/gliderlabs/logspout
 cd /go/src/github.com/gliderlabs/logspout
 export GOPATH=/go
 go get
-go build -ldflags "-X main.Version $1" -o /bin/logspout
+go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
 rm -rf /go
 rm -rf /var/cache/apk/*
