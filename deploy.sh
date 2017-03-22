e6b19766dd0c822c68de97c455c3dbd5aa36fe6d
Martin Halamíček <martin@halamicek.cz>
2017-03-22 13:29:18 +0100

    fix ecr deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ VERSION=`cat VERSION`
 echo $VERSION
 
 eval $(docker run --rm -i -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY quay.io/keboola/aws-cli ecr get-login --region us-east-1)
-docker tag keboola/logspout:$VERSION 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/logspout:latest
+docker tag logspout:$VERSION 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/logspout:latest
 docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/logspout:latest
-docker tag keboola/logspout:$VERSION 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/logspout:$TRAVIS_COMMIT
+docker tag logspout:$VERSION 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/logspout:$TRAVIS_COMMIT
 docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/logspout:$TRAVIS_COMMIT
