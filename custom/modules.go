2b26de453770f82d1f69bede9a9fd2836a85c409
Michael Hobbs <github@psudo.net>
2017-04-13 22:07:32 -0700

    build: add tls test case

--- custom/modules.go
+++ custom/modules.go
@@ -3,5 +3,7 @@ package main
 import (
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
+	_ "github.com/gliderlabs/logspout/transports/tls"
+	_ "github.com/gliderlabs/logspout/transports/udp"
 	_ "github.com/looplab/logspout-logstash"
 )
