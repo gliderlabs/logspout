a674c3367e2e5e3a464a0e53f8cd3a712da516db
fjohnson <fjohnson@peoplenetonline.com>
2017-10-05 15:29:36 -0500

    make build update glide.

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -e
 rsync -ar /src/ /go/src/github.com/gliderlabs/logspout --exclude glide.lock --exclude glide.yaml
 
 cd /go/src/github.com/gliderlabs/logspout
-ls -lah
+#ls -lah
 export GOPATH=/go
-$GOPATH/bin/glide install
+$GOPATH/bin/glide update && $GOPATH/bin/glide install
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
 rm -rf /go /var/cache/apk/* /root/.glide
