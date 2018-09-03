add91a569a8c91c8f05d313f82c10aaa6be8a381
Martin Halamíček <martin@halamicek.cz>
2018-09-03 13:25:23 +0200

    deploy to ap-southeast-2

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,10 @@ docker tag logspout:$VERSION 147946154733.dkr.ecr.eu-central-1.amazonaws.com/keb
 docker push 147946154733.dkr.ecr.eu-central-1.amazonaws.com/keboola/logspout:latest
 docker tag logspout:$VERSION 147946154733.dkr.ecr.eu-central-1.amazonaws.com/keboola/logspout:$TRAVIS_COMMIT
 docker push 147946154733.dkr.ecr.eu-central-1.amazonaws.com/keboola/logspout:$TRAVIS_COMMIT
+
+# ap-southeast-2
+eval $(docker run --rm -i -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY quay.io/keboola/aws-cli ecr get-login --region ap-southeast-2)
+docker tag logspout:$VERSION 147946154733.dkr.ecr.ap-southeast-2.amazonaws.com/keboola/logspout:latest
+docker push 147946154733.dkr.ecr.ap-southeast-2.amazonaws.com/keboola/logspout
+docker tag logspout:$VERSION 147946154733.dkr.ecr.ap-southeast-2.amazonaws.com/keboola/logspout:$TRAVIS_COMMIT
+docker push 147946154733.dkr.ecr.ap-southeast-2.amazonaws.com/keboola/logspout:$TRAVIS_COMMIT
